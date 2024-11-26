1a3e7b2e75b4887275666cbcda1f4c774c52feae
Christopher Hogg <cthogg@users.noreply.github.com>
2024-11-26 21:19:07 +0000

    feat: make more

---
+++ linksToPrompt/index.ts
@@ -0,0 +1,22 @@
+// Do for all here https://en.wikipedia.org/wiki/Grade_II*_listed_buildings_in_the_London_Borough_of_Waltham_Forest
+
+const listedBuildingNumber = 1065590;
+const listedBuildingLink = `https://historicengland.org.uk/listing/the-list/list-entry/${listedBuildingNumber}`;
+const wikipediaLink = "https://en.wikipedia.org/wiki/Vestry_House_Museum";
+const wikidataLink = "https://www.wikidata.org/wiki/Q1043896";
+
+// I will give you a historic england website https://historicengland.org.uk/listing/the-list/list-entry/1065590
+// A wikipedia link
+// An image
+// Can you do the following:
+
+// From the wikipedia link find the coordinates and output it as
+
+// get title
+// get grade of listed building
+// get coordinates of listed building
+// prompt is always the same
+
+// Get the listed building text
+// Get the wikipedia text
+// Output it like below in the markdown.
