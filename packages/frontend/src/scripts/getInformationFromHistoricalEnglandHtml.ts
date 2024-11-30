417637ccfdec79f9a077dbc04ab7a7b8cce7934d
Christopher Hogg <cthogg@users.noreply.github.com>
2024-11-30 12:43:15 +0000

    feat: get some things

--- packages/frontend/src/scripts/getInformationFromHistoricalEnglandHtml.ts
+++ packages/frontend/src/scripts/getInformationFromHistoricalEnglandHtml.ts
@@ -1,13 +1,27 @@
 import * as cheerio from "cheerio";
-import { siteHtml } from "./siteHtml";
 
-export const getInformationFromHistoricalEnglandHtml = async (
-  siteHtml: string
+const fetchSiteHtml = async (listedBuildingNumber: string) => {
+  const url = `https://historicengland.org.uk/listing/the-list/list-entry/${listedBuildingNumber}?section=official-list-entry`;
+  const response = await fetch(url);
+  const responseText = await response.text();
+  return responseText;
+};
+
+const getInformationFromHistoricEnglandNumber = async (
+  listedBuildingNumber: string
 ) => {
+  const siteHtml = await fetchSiteHtml(listedBuildingNumber);
+  console.log("siteHtml", siteHtml);
+  return getInformationFromHistoricalEnglandHtml(siteHtml);
+};
+
+const getInformationFromHistoricalEnglandHtml = async (siteHtml: string) => {
   const $ = cheerio.load(siteHtml);
+  const body = $("body");
 
   // Find the Details section and extract its text
-  const detailsSection = $(".nhle-entry__section-official--no-spacing-top")
+  const detailsSection = body
+    .find(".nhle-entry__section-official--no-spacing-top")
     .find('h3:contains("Details")')
     .parent()
     .find("p")
@@ -17,15 +31,14 @@ export const getInformationFromHistoricalEnglandHtml = async (
     // Replace <br> tags (which appear as spaces) with newlines
     .replace(/\s*<br>\s*/g, "\n")
     .trim();
-
   // Find the Sources section
-  const sourcesSection = $(".nhle-entry__section-official--no-spacing-top")
+  const sourcesSection = body
+    .find(".nhle-entry__section-official--no-spacing-top")
     .find('h3:contains("Sources")')
     .parent()
     .text()
     .replace(/\s+/g, " ")
     .trim();
-
   return {
     details: detailsSection,
     sources: sourcesSection,
@@ -34,5 +47,6 @@ export const getInformationFromHistoricalEnglandHtml = async (
 };
 
 console.log(
-  (await getInformationFromHistoricalEnglandHtml(siteHtml)).detailsAndSources
+  //FIXME: get some things
+  (await getInformationFromHistoricEnglandNumber("1190187")).detailsAndSources
 );
