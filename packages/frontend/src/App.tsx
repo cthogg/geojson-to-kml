90c15b146bf92560e670c1d3dd49d412426d3d40
Christopher Hogg <cthogg@users.noreply.github.com>
2024-11-30 10:47:55 +0000

    feat: get listed builidng from url

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import "./App.css";
 
+function getListedBuildingNumberFromUrl() {
+  const url = window.location.href;
+  const match = url.match(/listed-building\/(\d+)/);
+  return match ? match[1] : null;
+}
+
 function App() {
   const imageUrl =
     "https://private-user-images.githubusercontent.com/19980269/389188918-14239c1c-3d89-49aa-8751-76f4e309ca15.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzI5NjIzODYsIm5iZiI6MTczMjk2MjA4NiwicGF0aCI6Ii8xOTk4MDI2OS8zODkxODg5MTgtMTQyMzljMWMtM2Q4OS00OWFhLTg3NTEtNzZmNGUzMDljYTE1LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDExMzAlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMTMwVDEwMjEyNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTk0ZTQ4ODc3NzEyYWFmMGNlMzI3NzA5MTQxYjQ4NWE4MWZmMWQ0NGNjM2U2MGU2YjdhOWQyOGRiODg2Mzk5NTImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.Wh0zlQ48rkAkpzgD415K8SWGCmltSvOKh3BjV3OyfUE";
@@ -7,6 +13,7 @@ function App() {
     "https://github.com/user-attachments/assets/950b6541-dbfc-42b1-94c7-6f77b0bf4d11";
   const description = "Here is a description of the page";
   const title = "Victoria and Albert Museum";
+  const listedBuildingNumber = getListedBuildingNumberFromUrl();
   return (
     <>
       {/* Sticky Header */}
@@ -24,6 +31,9 @@ function App() {
           <h1 className="text-4xl font-bold text-center text-gray-800">
             {title}
           </h1>
+          <p className="text-gray-700 leading-relaxed mt-2">
+            Listed Building Number: {listedBuildingNumber}
+          </p>
 
           {/* Audio Player */}
           <div className="bg-white p-6 rounded-lg shadow-md">
