eb6e4f8ced5573d9f23ecf8fb9d8afdd8b3d636e
Christopher Hogg <cthogg@users.noreply.github.com>
2024-12-09 18:01:37 +0000

    feat: more changes

--- packages/frontend/src/scripts/listedBuildingSources/listedBuildingFileTypes.ts
+++ packages/frontend/src/scripts/listedBuildingSources/listedBuildingFileTypes.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-/* eslint-disable @typescript-eslint/no-unused-vars */
 export const ListedBuildingSchema = z.object({
   title: z.string(),
   type: z.string(),
@@ -13,6 +12,5 @@ export const ListedBuildingSchema = z.object({
 });
 
 export const ListedBuildingArraySchema = z.array(ListedBuildingSchema);
-/* eslint-enable @typescript-eslint/no-unused-vars */
 
 export type ListedBuilding = z.infer<typeof ListedBuildingSchema>;
