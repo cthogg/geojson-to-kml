d855129addd440d81747981462b2bddea2d754e9
Christopher Hogg <cthogg@users.noreply.github.com>
2024-12-10 19:54:03 +0000

    feat: more references

--- packages/frontend/src/reactMap/listedBuildingsGeojsonTypes.ts
+++ packages/frontend/src/reactMap/listedBuildingsGeojsonTypes.ts
@@ -1,25 +1,7 @@
 import data from "../../../backend/kmlGeneration/input/filtered-listed-buildings.json";
 
-// {
-//     "dataset": "listed-building",
-//     "end-date": "",
-//     "entity": "31479762",
-//     "entry-date": "2023-05-25",
-//     "geometry": "",
-//     "name": "CRESCENT HOUSE INCLUDING GROUND FLOOR SHOPS AND SHAKESPEARE PUBLIC HOUSE",
-//     "organisation-entity": "16",
-//     "point": "POINT (-0.097280 51.522372)",
-//     "prefix": "listed-building",
-//     "reference": "1021941",
-//     "start-date": "1997-12-04",
-//     "typology": "geography",
-//     "documentation-url": "https://historicengland.org.uk/listing/the-list/list-entry/1021941",
-//     "listed-building-grade": "II*",
-//     "latitude": 51.522372,
-//     "longitude": -0.09728
-//   },
-
 import { z } from "zod";
+import { getListedBuildingFileFE } from "../scripts/listedBuildingSources/getListedBuildingFE";
 
 const ListedBuildingGeojsonSchema = z.object({
   "listed-building-grade": z.string(),
@@ -32,6 +14,10 @@ const ListedBuildingGeojsonSchema = z.object({
 export type ListedBuildingGeojson = z.infer<typeof ListedBuildingGeojsonSchema>;
 
 export const getListedBuildingGeojson = (): ListedBuildingGeojson[] => {
-  const geo = z.array(ListedBuildingGeojsonSchema).parse(data);
+  const allSources = getListedBuildingFileFE().map((s) => s.listEntry);
+  const geo = z
+    .array(ListedBuildingGeojsonSchema)
+    .parse(data)
+    .filter((geo) => allSources.includes(geo.reference));
   return geo;
 };
