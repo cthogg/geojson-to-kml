25f2cbb77cc9a89b2a3012c8241cedebe40722ea
Christopher Hogg <cthogg@users.noreply.github.com>
2024-12-23 20:40:31 +0000

    feat: get a main

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,46 +1,8 @@
 import "./App.css";
-import { ListedBuilding } from "./backendSync/listedBuildingFileTypes";
-import { ListedBuildingInfo } from "./ListedBuildingInfo";
 import { Map } from "./Map";
-import { getListedBuildingFileFE } from "./scripts/listedBuildingSources/getListedBuildingFE";
-
-function getListedBuildingNumberFromRoute() {
-  const url = window.location.href;
-  const match = url.match(/listed-building\/(\d+)/);
-  return match ? match[1] : null;
-}
-function getListedBuildingInformation(
-  listedBuildingNumber: string
-): ListedBuilding | undefined {
-  const buildings = getListedBuildingFileFE();
-  const listedBuilding = buildings.find(
-    (listedBuilding) => listedBuilding.listEntry === listedBuildingNumber
-  );
-  return listedBuilding;
-}
 
 function App() {
-  const listedBuildingNumber = getListedBuildingNumberFromRoute();
-  if (!listedBuildingNumber) {
-    return <Map />;
-  }
-  const listedBuildingInformation =
-    getListedBuildingInformation(listedBuildingNumber);
-  if (!listedBuildingInformation) {
-    return <div>No listed building information found</div>;
-  }
-
-  return (
-    <>
-      <ListedBuildingInfo
-        title={listedBuildingInformation.title}
-        imageUrl={listedBuildingInformation.imageUrl}
-        wikipediaText={listedBuildingInformation.wikipediaText}
-        historicalEnglandText={listedBuildingInformation.historicalEnglandText}
-        listedBuildingNumber={listedBuildingNumber}
-      />
-    </>
-  );
+  return <Map />;
 }
 
 export default App;
