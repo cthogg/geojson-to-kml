cb9955070404cf0643ab673e1510d43ea1bbe455
Christopher Hogg <cthogg@users.noreply.github.com>
2024-12-24 10:12:42 +0000

    feat: hono api

---
+++ packages/api/src/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/posts/:id", (c) => {
+  const page = c.req.query("page");
+  const id = c.req.param("id");
+  c.header("X-Message", "Hi!");
+  return c.text(`You want see ${page} of ${id}`);
+});
+
+export default app;
