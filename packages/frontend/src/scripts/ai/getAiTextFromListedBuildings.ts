810e92dc7d6250c58147056d862b7e39ecff9824
Christopher Hogg <cthogg@users.noreply.github.com>
2024-12-31 16:56:25 +0000

    feat: more prompt

--- packages/frontend/src/scripts/ai/getAiTextFromListedBuildings.ts
+++ packages/frontend/src/scripts/ai/getAiTextFromListedBuildings.ts
@@ -7,10 +7,10 @@ const systemPrompt =
   "You are an architectural tour guide, giving a tour to a person with a lay interest in historical architecture. Describe the listed building in this text and images, pointing out specific features on the building to look out for. Please describe the as if you are standing in front of it from the perspective of the image. Do not use phrases like 'Points to decorative details' => but instead phrases like 'look at the decorative details'. Can you start with Welcome to. Please keep the answer to under 200 words.";
 
 const model: Anthropic.Messages.Model = "claude-3-5-haiku-20241022";
-const BATCH_SIZE = 4;
+const BATCH_SIZE = 20;
 
 export const getAiTextFromListedBuildings = async () => {
-  const buildings = getListedBuildingFileFE().slice(0, 116);
+  const buildings = getListedBuildingFileFE().slice(0, 400);
   const promptDb = getPromptData();
   const filteredPromptDb = buildings.filter(
     (building) =>
