b020cdb6311116059f6ca3b0f4d195d9287699c0
Christopher Hogg <cthogg@users.noreply.github.com>
2025-01-01 19:15:09 +0000

    feat: use supabase constants

--- packages/backend/listedBuildingSources/beSyncListedBuildingSources/getListedBuildingFE.ts
+++ packages/backend/listedBuildingSources/beSyncListedBuildingSources/getListedBuildingFE.ts
@@ -2,20 +2,16 @@ import {
   ListedBuilding,
   ListedBuildingArraySchema,
 } from "./listedBuildingFileTypes";
+import { SUPABASE_API_KEY, SUPABASE_URL } from "./supabaseApiKey";
 
 //FIXME: need to just get the basic id and latitude and longitude. Only on click should get full data.
 const fetchListedBuildings = async () => {
-  const response = await fetch(
-    "https://yvrmwbxbhglycwnckely.supabase.co/rest/v1/places",
-    {
-      headers: {
-        apikey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2cm13YnhiaGdseWN3bmNrZWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MjU0ODksImV4cCI6MjA1MTMwMTQ4OX0.YXu8R2oQIB4tXsIcrqkv1S8W2lTEr9B8kVRVKptugbU",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2cm13YnhiaGdseWN3bmNrZWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MjU0ODksImV4cCI6MjA1MTMwMTQ4OX0.YXu8R2oQIB4tXsIcrqkv1S8W2lTEr9B8kVRVKptugbU",
-      },
-    }
-  );
+  const response = await fetch(`${SUPABASE_URL}/places`, {
+    headers: {
+      apikey: SUPABASE_API_KEY,
+      Authorization: `Bearer ${SUPABASE_API_KEY}`,
+    },
+  });
   const data = await response.json();
   return data;
 };
