720010024c93112bac5a50b616db6a775f4ac15d
Christopher Hogg <cthogg@users.noreply.github.com>
2025-01-01 19:13:59 +0000

    feat: put supabase to new part

---
+++ packages/frontend/src/scripts/beSyncListedBuildingSources/supabaseApiKey.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_API_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2cm13YnhiaGdseWN3bmNrZWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MjU0ODksImV4cCI6MjA1MTMwMTQ4OX0.YXu8R2oQIB4tXsIcrqkv1S8W2lTEr9B8kVRVKptugbU";
+export const SUPABASE_URL = "https://yvrmwbxbhglycwnckely.supabase.co/rest/v1";
