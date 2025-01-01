f3159fd6291c9d98cc342dffe0c84de3cab391fb
Christopher Hogg <cthogg@users.noreply.github.com>
2025-01-01 18:02:09 +0000

    feat: write more summaries

--- packages/backend/listedBuildingSources/ai/getAiTextFromListedBuildings.ts
+++ packages/backend/listedBuildingSources/ai/getAiTextFromListedBuildings.ts
@@ -8,24 +8,22 @@ const systemPrompt =
   "You are an architectural tour guide, giving a tour to a person with a lay interest in historical architecture. Describe the listed building in this text and images, pointing out specific features on the building to look out for. Please describe the as if you are standing in front of it from the perspective of the image. Do not use phrases like 'Points to decorative details' => but instead phrases like 'look at the decorative details'. Can you start with Welcome to. Please keep the answer to under 200 words.";
 
 const model: Anthropic.Messages.Model = "claude-3-5-haiku-20241022";
-const BATCH_SIZE = 1;
+const BATCH_SIZE = 20;
 
 export const getAiTextFromListedBuildings = async () => {
   const prompts = await getPrompts();
   const buildings = await getListedBuildingFileBE();
   const promptDb = await getAiSummaries();
-  const filteredPromptDb = buildings
-    .filter(
-      (building) =>
-        !promptDb.some(
-          (prompt) =>
-            prompt.list_entry === building.list_entry &&
-            prompts.find((p) => p.id === prompt.prompt)?.prompt ===
-              systemPrompt &&
-            prompt.model === model
-        )
-    )
-    .slice(0, 2);
+  const filteredPromptDb = buildings.filter(
+    (building) =>
+      !promptDb.some(
+        (prompt) =>
+          prompt.list_entry === building.list_entry &&
+          prompts.find((p) => p.id === prompt.prompt)?.prompt ===
+            systemPrompt &&
+          prompt.model === model
+      )
+  );
   console.log(
     `Filterting out ${buildings.length - filteredPromptDb.length} buildings`
   );
