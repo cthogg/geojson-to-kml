427bae2d6bba8a14d62c6ab282afb78b1c5119ae
Christopher Hogg <cthogg@users.noreply.github.com>
2025-02-18 19:46:03 +0000

    fet: more

--- packages/frontend/src/WikipediaPanel.tsx
+++ packages/frontend/src/WikipediaPanel.tsx
@@ -136,9 +136,37 @@ export function WikipediaPanel({
             {playAudioMutation.isPending ? (
               <span className="animate-spin">⟳</span>
             ) : isPlaying ? (
-              "⏸"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <g fill="currentColor">
+                  <path
+                    fill-rule="evenodd"
+                    d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2M5 1a4 4 0 0 0-4 4v14a4 4 0 0 0 4 4h14a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4z"
+                    clip-rule="evenodd"
+                  />
+                  <path d="M8 7h2v10H8zm6 0h2v10h-2z" />
+                </g>
+              </svg>
             ) : (
-              "▶"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <g fill="currentColor">
+                  <path
+                    fill-rule="evenodd"
+                    d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2M5 1a4 4 0 0 0-4 4v14a4 4 0 0 0 4 4h14a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4z"
+                    clip-rule="evenodd"
+                  />
+                  <path d="m16 12l-6 4.33V7.67z" />
+                </g>
+              </svg>
             )}
           </button>
         </h2>
